69de6ee6c270d167ed418d813dd51496d9a4f519
NumanZaidan <92940612+NumanZaidan@users.noreply.github.com>
2024-11-10 17:18:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
 function showSection(sectionId) {
     document.querySelectorAll('.content-section').forEach(section => {
-        section.style.display = 'none';
+        if (section.id === sectionId) {
+            gsap.to(section, {duration: 1, autoAlpha: 1, display: 'block'});
+        } else {
+            gsap.to(section, {duration: 1, autoAlpha: 0, display: 'none'});
+        }
     });
-    document.getElementById(sectionId).style.display = 'block';
 }
 
 function playMovie(videoSrc) {
     document.querySelectorAll('.content-section').forEach(section => {
-        section.style.display = 'none';
+        gsap.to(section, {duration: 1, autoAlpha: 0, display: 'none'});
     });
     const videoPlayer = document.getElementById('videoPlayer');
     videoPlayer.src = videoSrc;
-    document.getElementById('player').style.display = 'flex';
+    gsap.to('#player', {duration: 1, autoAlpha: 1, display: 'flex'});
 }
 
 function closePlayer() {
     const videoPlayer = document.getElementById('videoPlayer');
     videoPlayer.src = ''; // Clear the source to stop the video
-    document.getElementById('player').style.display = 'none';
+    gsap.to('#player', {duration: 1, autoAlpha: 0, display: 'none'});
     showSection('movies');
 }
