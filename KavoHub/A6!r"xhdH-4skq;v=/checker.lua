1c3f639c3353bcaac033e91642edf87632ef2f83
NumanZaidan <92940612+NumanZaidan@users.noreply.github.com>
2025-04-05 14:31:14 +0800

    Create checker.lua

---
+++ KavoHub/A6!r"xhdH-4skq;v=/checker.lua
@@ -0,0 +1,9 @@
+if game.GameId == 536610775 then
+    warn("DEBUG | ScriptFusionX Hub Loader | Supported Game | Loading Game | The Lanes Bowling")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NumanZaidan/ScriptFusionX-Hub/refs/heads/main/ScriptFusionX%20Hub%20%7C%20The%20Lanes%20Bowling.lua",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NumanZaidan/ScriptFusionX-Hub/refs/heads/main/ScriptFusionX%20Hub%20%7C%20The%20Lanes%20Bowling.lua",true))()
+elseif game.GameId == 7267853694 then
+    warn("DEBUG | ScriptFusionX Hub Loader | Supported Game | Loading Game | Building Boats")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NumanZaidan/ScriptFusionX-Hub/refs/heads/main/ScriptFusionX%20Hub%20%7C%20Building%20Boats",true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/NumanZaidan/ScriptFusionX-Hub/refs/heads/main/ScriptFusionX%20Hub%20%7C%20Building%20Boats",true))()
+end
